10c40a66780a82df197d07c30d52f59032d5f281
wyn155 <53067080+wyn155@users.noreply.github.com>
2020-01-16 12:53:56 +0800

    updata

--- CRM/api.py
+++ CRM/api.py
@@ -1,8 +1,8 @@
 from mymain import api
 from myuris.searchuri import CustomerAPI
-from myuris.often import often_CustomerAPI
+# from myuris.often import often_CustomerAPI
 
 
 def register_api():
     api.add_resource(CustomerAPI,"/api/customer", endpoint="customer")
-    api.add_resource(often_CustomerAPI,"/api/oftencustomer",endpoint="oftencustomer")
+    # api.add_resource(often_CustomerAPI,"/api/oftencustomer",endpoint="oftencustomer")
--- CRM/myuris/searchuri.py
+++ CRM/myuris/searchuri.py
@@ -5,26 +5,32 @@ from flask import render_template,request,redirect
 import json
 
 # 返回资源
+@search.route("/")
+def index():
+    return render_template("search.html")
 
 
 class CustomerAPI(Resource):
-    @search.route("/", methods=["GET", "POST"])
+    @search.route("/search", methods=["GET", "POST"])
     def get(self):
         customer_code = request.args.get("customer_code")
-        name = request.args.get('customer_name','NONE')
+        print(customer_code)
+        name = request.args.get("customer_name","NONE")
 
 
         data = []
         con = []
-        data_code = Customer.query.filter_by(customer_code = 20615).all()
-        data_name = Customer.query.filter(Customer.customer_name.like("%"+name+"%")).all()
-
-        data.append(data_name)
+        data_code = Customer.query.filter_by(customer_code = customer_code).all()
+        # data_name = Customer.query.filter(Customer.customer_name.like("%"+name+"%")).all()
+        # data.append(data_name)
         data.append(data_code)
         for item in data:
             for i in item:
                 d = i.__dict__
-                d.pop('_sa_instance_state')
+                d.pop("_sa_instance_state")
+                # d = str(d)
+                print(d)
+                # d = json.loads(d)
                 con.append(d)
 
 
@@ -32,10 +38,3 @@ class CustomerAPI(Resource):
         # return json.dumps({'code':200,'msg':'ok','data':con})
 
 
-# 常用客户 ['滴滴','联影','大华','韶音','蚂蚁']
-# def regular_customers(self):
-#     clien_to = ['滴滴','联影','大华','韶音','蚂蚁']
-#     data_code = Customer.query.filter_by('{}').all().format(clien_to)
-#     print(data_code)
-#
-#     return {'data' : data_code}
